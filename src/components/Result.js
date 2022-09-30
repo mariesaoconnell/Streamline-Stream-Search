c8860a5a5031408f0c8917ffba18e131cca7c113
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-09-29 22:27:53 -0400

    Successful Fetch

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 function Result({search}) {
-  
 
-  return (
-    <div>
-      {search}
-    </div>
-  );
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+	},
+};
+
+const url = `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${search}&country=us&type=all&output_language=en`;
+
+fetch(url, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
 }
-
 export default Result;
