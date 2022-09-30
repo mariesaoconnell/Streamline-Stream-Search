a57aa47fbbd50ad52829ac61f258403c4cab596d
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-09-29 21:42:53 -0400

    added form.. form functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import './app-style.css';
+import Form from './components/Form';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+  // STATE
+  const [search, setSearch] = useState('');
+
+  return (
+		<div className='app-main-div'>
+			<nav>
+				<h1>logo</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+			</nav>
+
+      <main>
+        <Form search={search} setSearch={setSearch}/>
+      </main>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import {useState} from 'react';
+import './styles/form.css'
 
-function Form(props) {
+
+function Form({search, setSearch}) {
+
+  // FORM STATE
+  const [newSearch, setNewSearch] = useState('');
+
+
+  // HANDLE SUBMIT FUNCTION
+  function handleSubmit(event){
+		event.preventDefault();
+		console.log('test');
+
+		//CHANGE APP STATE
+		setSearch([...search, newSearch]);
+		setNewSearch('');
+	}
+
+  // HANDLE CHANGE FUNCTION
+  function handleChange(event){
+  // set search to string input
+    setNewSearch(event.target.value);
+  }
+
   return (
-    <div>
-      
-    </div>
-  );
+		<div id='form-main-div'>
+			<h1 id='form-headline'>Streamline</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input onChange={handleChange} type='text' value={newSearch}></input>
+				<button type="submit">Search</button>
+			</form>
+		</div>
+	);
 }
 
 export default Form;
